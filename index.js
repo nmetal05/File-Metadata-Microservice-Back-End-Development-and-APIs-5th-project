9325dd13278b2ccf071a835030eddcd460eedd5d
nmetal05 <nmetal05@gmail.com>
2024-02-11 15:01:48 +0100

    first commit

--- index.js
+++ index.js
@@ -1,20 +1,34 @@
-var express = require('express');
-var cors = require('cors');
-require('dotenv').config()
-
-var app = express();
-
-app.use(cors());
-app.use('/public', express.static(process.cwd() + '/public'));
-
-app.get('/', function (req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+const express = require("express");
+const cors = require("cors");
+const multer = require("multer");
+require("dotenv").config();
+const app = express();
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "uploads/");
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + "-" + file.originalname);
+  },
 });
 
+const upload = multer({ storage: storage });
 
+app.use(cors());
+app.use("/public", express.static(process.cwd() + "/public"));
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
+});
+app.post("/api/fileanalyse", upload.single("upfile"), (req, res) => {
+  const fileMetadata = {
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size,
+  };
+  res.json(fileMetadata);
+});
 
-
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+  console.log("Your app is listening on port " + port);
 });
